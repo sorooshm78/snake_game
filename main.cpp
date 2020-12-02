b4cbfb588a31f2788daed435df75421ba48cffc5
sm <sorooshm78@gmail.com>
2020-12-02 19:08:51 +0330

    putting up food in game page

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <stdlib.h>
 #include <string>
 #include <vector>
+#include <time.h>
 
 using namespace std;
 
@@ -37,12 +39,28 @@ void initialize_margins_page(vector<vector<string>>& page, string& margins)
 
 void initialize_snake_in_page(vector<vector<string>>& page, string& snake, int snake_size)
 {
-	int y = page[0].size() / 2;
-	int x = page.size() / 2;
-	// snake head
-	page[x][y] = "<";
+	int x_snake = page[0].size() / 2;
+	int y_snake = page.size() / 2;
+
 	for(int i = 1; i < snake_size ; i++)
-		page[x][y + i] = snake;
+		page[y_snake][x_snake + i] = snake;
+}
+
+void initialize_food_in_page(vector<vector<string>>& page, string& inside, string& food)
+{
+	int lenght = page[0].size();
+	int width = page.size();
+
+	while(true)
+	{	
+		int x_food = rand() % lenght;
+		int y_food = rand() % width;
+		if(page[y_food][x_food] == inside)
+		{	
+			page[y_food][x_food] = food;
+			return;
+		}
+	}
 }
 
 void print_page(vector<vector<string>>& page)
@@ -59,17 +77,22 @@ void print_page(vector<vector<string>>& page)
 
 int main()
 {
+	// Srand
+	srand(time(0));
+
 	// Confegur setting
+	vector <vector<string>> page;
+	string margins = "#";
+	string inside = " ";
+	string snake = ">";
+	string food = "*";
 	int length_page = 30;
 	int width_page = 20;
-	string page_inside = " ";
-	string page_margins = "#";
-	string snake = "-";
 	int snake_size = 5;
-	vector <vector<string>> page;
 
-	initialize_page(page, length_page, width_page, page_inside);
-	initialize_margins_page(page, page_margins);
+	initialize_page(page, length_page, width_page, inside);
+	initialize_margins_page(page, margins);
 	initialize_snake_in_page(page, snake, snake_size);
+	initialize_food_in_page(page, inside, food);
 	print_page(page);
 }
