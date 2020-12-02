df114836cb74aed818464a5ec7e51bf38c25dfd1
sm <sorooshm78@gmail.com>
2020-12-02 16:59:49 +0330

    creat game page

--- main.cpp
+++ main.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
+void initialize_page(vector<vector<string>>& point, int lenght, int width)
+{
+	vector <string> x_page(lenght, ".");
+	for(int i = 0; i < width; i++)
+	{
+		point.push_back(x_page);
+	}
+}
+
+void print_page(vector<vector<string>>& point)
+{
+	for(int i = 0; i < point.size(); i++)
+	{
+		for(int j = 0; j < point[i].size(); j++)
+		{	
+			cout << point[i][j] << " " ;
+		}
+		cout << endl;
+	}
+}
+
 int main()
 {
- 
+	// Confegur setting
+	int length_page = 20;
+	int width_page = 10;
+	vector <vector<string>> page;
+	initialize_page(page, length_page, width_page);
+	print_page(page);
 }
