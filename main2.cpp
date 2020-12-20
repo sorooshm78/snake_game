4407a0ecdaecb6fd9331ff120e84d177e922be81
sorooshm78@gmail.com <sorooshm78@gmail.com>
2020-12-20 20:27:28 +0330

    .

--- main2.cpp
+++ main2.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <time.h>
+#include <stdlib.h>
 
 using namespace std;
 
@@ -15,17 +17,34 @@ snake(int x, int y)
 	}
 }
 
+void move()
+{
+
+
+}
+
 //private:
 	vector<pair<int, int>> coordinates;
-	char shape = '*';
+	char shape = '+';
 	int primitive_size = 5;
 	int score = 0;
+	string move_type = "left";
+};
+
+class food
+{
+public:
+
+//private:
+	vector<pair<int, int>> coordinates;
+	int count = 1;
+	char shape = '*';
 };
 
 class page
 {
 public:
-void print(snake *s)
+void print(snake *s, food *f)
 {
 	for(int y = 0; y < width; y++)
 	{
@@ -50,6 +69,17 @@ void print(snake *s)
 				}
 			}
 
+			// food 
+			for(int i = 0; i < f->coordinates.size(); i++)
+			{
+				if(x == f->coordinates[i].first and y == f->coordinates[i].second)
+				{
+					print_empty = false;
+					cout << f->shape << " ";
+					break;
+				}
+			}
+
 			if(print_empty)
 			{
 				cout << empty_shape << " ";
@@ -59,6 +89,9 @@ void print(snake *s)
 	}
 }
 
+int get_lenght(){ return lenght;}
+int get_width(){ return width;}
+
 private:
 	int lenght = 30;
 	int width = 20;
@@ -66,18 +99,41 @@ private:
 	char empty_shape = '.';
 };
 
-class food
+bool check_corruct_coordinates(int x_food, int y_food, snake *s)
 {
-public:
-private:
-	vector<pair<int, int>> coordinates;
-	int count = 1;
-	char shape = '*';
-};
+	for(int i = 0; i < s->coordinates.size(); i++)
+	{
+		if(x_food == s->coordinates[i].first and y_food == s->coordinates[i].second)
+			return false;		
+	}
+	return true;
+}
+
+void insert_food(page *p, snake *s, food *f)
+{
+	for(int i = 0; i < f->count; i++)
+	{
+        while(true)
+        {
+			int x_food = rand() % (p->get_lenght() - 3) + 2;
+            int y_food = rand() % (p->get_width() - 3) + 2;
+							
+			if(check_corruct_coordinates(x_food, y_food, s))
+	        {	
+				f->coordinates.push_back(pair<int, int>(x_food, y_food));
+				break;
+			}
+        }			
+	}
+}
 
 int main()
 {
+	srand(time(0));	
+
 	snake s(5, 5);
+	food f;
 	page p;
-	p.print(&s);
+	insert_food(&p, &s, &f);
+	p.print(&s, &f);
 }
